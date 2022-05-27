f7a859d00ce90ce217d39069bf483121191b4f32
chou3934 <choumelanie12@gmail.com>
2022-05-27 13:01:12 +0800

    16th commit

--- src/main/java/com/bob/ui/GuessFrame.java
+++ src/main/java/com/bob/ui/GuessFrame.java
@@ -4,37 +4,54 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame  extends JFrame {
 
-
-
+    JButton button=new JButton("guess");
+    JLabel label=new JLabel("Guess a number from 1 to 10.");
+    JTextField number=new JTextField(10);
     public GuessFrame(){
         super();
         setSize(400,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
-        JButton button=new JButton("Hi");
-        JLabel label=new JLabel("wow");
+        setLayout(new FlowLayout());
+
+        add(label);
+        add(number);
+        add(button);
+
+        Random r=new Random();
+        int secret=r.nextInt(10);
+        System.out.println("secret: "+secret);
+
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("hi");
-                label.setText("yeah");
+//                System.out.println("hi");
+//                label.setText("yeah");
+                int num= Integer.parseInt(number.getText());
+                System.out.println("answer: "+num);
+
+                if(num==secret){
+                    System.out.println("You win!");
+                }
+                else {
+                    if(num>secret){
+                        System.out.println("smaller");
+                    }
+                    else{
+                        System.out.println("bigger");
+                    }
+                }
+
             }
         });
-
-        setLayout(new FlowLayout());
-        add(button);
     }
 
-
-
-
-
     public static void main(String[] args) {
-        GuessFrame frame=new GuessFrame();
-
+        GuessFrame frame= new GuessFrame();
     }
 }
