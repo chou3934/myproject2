44edcbf720ca3432c0f0559dc596cfd17ffb6cdf
chou3934 <choumelanie12@gmail.com>
2022-05-27 12:05:53 +0800

    Homework1(15th commit)

--- src/main/java/com/bob/ui/MyForm2.java
+++ src/main/java/com/bob/ui/MyForm2.java
@@ -1,7 +1,14 @@
 package com.bob.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyForm2 {
-    JButton button=new JButton();
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setVisible(true);
+        frame.setSize(400,400);
+        frame.setLocation(400,100);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
 }
