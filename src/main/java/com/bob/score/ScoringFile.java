427e7a3bdcfc34c65b8d9365b53bd8d6ad7db267
chou3934 <choumelanie12@gmail.com>
2022-06-17 12:58:19 +0800

    17th commit

---
+++ src/main/java/com/bob/score/ScoringFile.java
@@ -0,0 +1,38 @@
+package com.bob.score;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class ScoringFile {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("scores.text");
+            BufferedReader reader=new BufferedReader(fileReader);
+            String line=reader.readLine();
+            while(line!=null){
+                //System.out.println(line);
+                String[] token=line.split(",");
+                String name=token[0];
+                int english=Integer.parseInt(token[1]);
+                int math=Integer.parseInt(token[2]);
+                Student stu=new Student(name,english,math);
+                stu.print();
+                line=reader.readLine();
+            }
+
+            /*int data=fileReader.read();
+            while(data!=-1){
+            System.out.print((char)data);
+            data= fileReader.read();
+            }*/
+        } catch (FileNotFoundException ex){
+            System.out.println("檔案讀取失敗");
+        }catch (IOException e){
+            System.out.println("資料讀取失敗");
+        }
+
+    }
+}
